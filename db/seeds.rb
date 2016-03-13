d931940e16e7ea868bd2b14af152c1888a05e013
Michael Arnold <arnold.michael91@gmail.com>
2016-03-12 22:01:52 -0800

    added caroussel

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,9 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-5.times do 
-Product.create(:name => "seed-generated product", :description => "This was created by seeds.rb")
-end
+
+Product.create(:name => "Black Cat", :description => "This was created by seeds.rb", :price => 40, :color => "black", :image_url => "http://www.kapatcha.com/bilder/produkte/gross/Yupoong-Animal-Tone-in-Tone-Snapback-Black-Zebra_b6.jpg")
+
+Product.create(:name => "White Triangle", :description => "This was created by seeds.rb", :price => 45, :color => "white", :image_url => "https://cdn.shopify.com/s/files/1/0232/3067/products/tv-sum-white.jpg?v=1413306190")
+
+Product.create(:name => "Super Cap", :description => "This was created by seeds.rb", :price => 40, :color => "black/red", :image_url => "http://assets.craniumfitteds.com/images/main/2-California-Republic-Rasta-Colors-Custom-Snapback-950-9fifty-New-Era-Cap-2.jpg")
